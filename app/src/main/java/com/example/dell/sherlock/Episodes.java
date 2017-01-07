4f67a3a0efafadc8d3df8c7f1f27fd950b6f33de
Partitioners <aniketsingh.sachan@gmail.com>
2017-01-08 02:14:34 +0530

    reduced classes

--- app/src/main/java/com/example/dell/sherlock/Episodes.java
+++ app/src/main/java/com/example/dell/sherlock/Episodes.java
@@ -2,6 +2,7 @@ package com.example.dell.sherlock;
 
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -29,41 +30,49 @@ import static com.example.dell.sherlock.MainActivity.t;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class S01E02 extends Fragment {
+public class Episodes extends Fragment {
+private int []videos=new int[]{R.drawable.vs1e1,R.drawable.vs1e2,R.drawable.vs1e3,R.drawable.vs2e1,R.drawable.vs2e2,R.drawable.vs2e3,
+        R.drawable.vs3e1,R.drawable.vs3e2,R.drawable.vs3e3};
+    private String []urls=new String[]{"https://www.youtube.com/watch?v=704AblLNlY8","https://www.youtube.com/watch?v=y_GGbRkqqFg",
+            "https://www.youtube.com/watch?v=AviDWKhmVdU","https://www.youtube.com/watch?v=E2MXppyXsUY","https://www.youtube.com/watch?v=bm78r2innnE",
+            "https://www.youtube.com/watch?v=eKRCt3yCXEA","https://www.youtube.com/watch?vO7cKIjNIPoY","https://www.youtube.com/watch?vqtGf6RvjWE4",
+            "https://www.youtube.com/watch?vxhjIsu7n6bI"};
+    private int []images=new int[]{R.drawable.s01e01,R.drawable.s01e02,R.drawable.s01e03,R.drawable.s02e01,R.drawable.s02e02,R.drawable.s02e03,
+            R.drawable.s03e01,R.drawable.s03e02,R.drawable.s03e03,};
 
-     private int ids=1;
     MyDBhandler db;
-
-    public S01E02() {
+private int index;
+    public Episodes() {
         // Required empty public constructor
     }
-
+public Episodes(int index)
+{this.index=index;}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle("SHERLOCK : SEASON 1");
-        View rootView = inflater.inflate(R.layout.episodes, container, false); //
-//
+        View rootView = inflater.inflate(R.layout.episodes, container, false);
+
         iv=(ImageView) rootView.findViewById(R.id.video);
-        iv.setImageResource(R.drawable.vs1e2);
+        iv.setImageResource(videos[index]);
         for(int i=0;i<2;i++) {
             iv1=(ImageView)rootView.findViewById(bs[i]);
             iv1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                  Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.youtube.com/watch?v=y_GGbRkqqFg"));
+                  Intent i=new Intent(Intent.ACTION_VIEW,Uri.parse(urls[index]));
                     startActivity(i);
                 }
             });
         }
         db=new MyDBhandler(getActivity());
         ImageView imageView=(ImageView)rootView.findViewById(R.id.image1);
-        imageView.setImageResource(R.drawable.s01e02);
+        imageView.setImageResource(images[index]);
 
         for(int i=0;i<7;i++) {
             t = (TextView) rootView.findViewById(a[i]);
-            t.setText(db.retval(ids,i));
+            t.setText(db.retval(index,i));
         }
 
 
--- app/src/main/java/com/example/dell/sherlock/season1frafadapt.java
+++ app/src/main/java/com/example/dell/sherlock/season1frafadapt.java
@@ -1,8 +1,6 @@
 package com.example.dell.sherlock;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -31,29 +29,12 @@ public class season1frafadapt extends FragmentPagerAdapter {
 
 
         if (SeasonNo == 0) {
-            if(position==0)
-            return new S01E01();
-        else if(position==1)
-                return new S01E02();
-            else
-                return new S01E03();
-
+            return new Episodes(position);
         } else if (SeasonNo == 1) {
-            if(position==0)
-                return new S02E01();
-            else if(position==1)
-                return new S02E02();
-            else
-                return new S02E03();
+            return new Episodes(position+3);
 
         } else  {
-            if(position==0)
-                return new S03E01();
-            else if(position==1)
-                return new S03E02();
-            else
-                return new S03E03();
-
+            return new Episodes(position+6);
         }
 
     }
